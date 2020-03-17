038bedc75c8d771df06c1ddf77d37ee0b3bc9cd1
Vladislav839 <55688058+Vladislav839@users.noreply.github.com>
2020-03-17 17:11:21 +0300

    Create Source.cpp

---
+++ lab7/Source.cpp
@@ -0,0 +1,18 @@
+#include <cctype>
+#include <iostream>
+
+extern "C" {
+int __cdecl TrimAll(char* str);
+}
+
+
+int main() {
+  char str[] = "   bsndb   j ihijoi   jiojilk  lkhlkhl   ";
+  std::cout << strlen(str) << std::endl;
+  std::cout << str << std::endl;
+  int res = TrimAll(str);
+  std::cout << str << std::endl;
+  std::cout << res << std::endl;
+  system("pause");
+  return 0;
+}
